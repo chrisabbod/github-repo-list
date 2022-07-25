390bd664bf85ebbafa33a43a249c0806e5999cb8
chris <chrisabbod@gmail.com>
2022-07-25 17:06:31 -0400

    Add recyclerview to main activity

--- app/src/main/java/com/chrisabbod/githubrepolist/MainActivity.kt
+++ app/src/main/java/com/chrisabbod/githubrepolist/MainActivity.kt
@@ -2,10 +2,29 @@ package com.chrisabbod.githubrepolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.chrisabbod.githubrepolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val items = listOf(
+        "JetBrains/kotlin - The Kotlin Programming Language",
+        "exercism/kotlin - Exercism exercises in Kotlin",
+        "cbeust/kobalt - A Kotlin-based build system for the JVM",
+        "JetBrains/kotlin - The Kotlin Programming Language",
+        "exercism/kotlin - Exercism exercises in Kotlin",
+        "cbeust/kobalt - A Kotlin-based build system for the JVM",
+        "JetBrains/kotlin - The Kotlin Programming Language"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvRepoList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.rvRepoList.adapter = RepoListAdapter(items)
     }
 }
---
+++ app/src/main/java/com/chrisabbod/githubrepolist/RepoListAdapter.kt
@@ -0,0 +1,26 @@
+package com.chrisabbod.githubrepolist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.chrisabbod.githubrepolist.databinding.ItemRepoBinding
+
+class RepoListAdapter (private val items: List<String>) : RecyclerView.Adapter<RepoListAdapter.RepoListViewHolder>() {
+
+    inner class RepoListViewHolder(itemBinding: ItemRepoBinding)
+        :RecyclerView.ViewHolder(itemBinding.root) {
+            val tvTitle = itemBinding.tvTitle
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoListViewHolder {
+        return RepoListViewHolder(ItemRepoBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RepoListViewHolder, position: Int) {
+        holder.tvTitle.text = items[position]
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
