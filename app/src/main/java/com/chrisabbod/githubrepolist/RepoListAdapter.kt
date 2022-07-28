a5589cb9da76d7b5b8922433d500e552d0235fc8
chris <chrisabbod@gmail.com>
2022-07-27 22:19:46 -0400

    Update list item layout item_repo

--- app/src/main/java/com/chrisabbod/githubrepolist/RepoListAdapter.kt
+++ app/src/main/java/com/chrisabbod/githubrepolist/RepoListAdapter.kt
@@ -5,15 +5,24 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.chrisabbod.githubrepolist.databinding.ItemRepoBinding
 
-class RepoListAdapter (private val items: List<String>) : RecyclerView.Adapter<RepoListAdapter.RepoListViewHolder>() {
+class RepoListAdapter(private val items: List<String>) :
+    RecyclerView.Adapter<RepoListAdapter.RepoListViewHolder>() {
 
-    inner class RepoListViewHolder(itemBinding: ItemRepoBinding)
-        :RecyclerView.ViewHolder(itemBinding.root) {
-            val tvTitle = itemBinding.tvTitle
-        }
+    inner class RepoListViewHolder(itemBinding: ItemRepoBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
+        val tvTitle = itemBinding.tvId
+        val tvName = itemBinding.tvName
+        val tvDescription = itemBinding.tvDescription
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoListViewHolder {
-        return RepoListViewHolder(ItemRepoBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return RepoListViewHolder(
+            ItemRepoBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: RepoListViewHolder, position: Int) {
