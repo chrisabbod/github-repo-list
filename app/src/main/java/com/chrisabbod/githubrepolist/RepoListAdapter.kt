10cd745598a0112965c316b4f006389c08f0d2f0
Christopher <chrisabbod@gmail.com>
2022-09-03 15:32:13 -0400

    Bug Fix: Adapter viewholder now has correct data to display on screen

--- app/src/main/java/com/chrisabbod/githubrepolist/RepoListAdapter.kt
+++ app/src/main/java/com/chrisabbod/githubrepolist/RepoListAdapter.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.chrisabbod.githubrepolist.data.Data
 import com.chrisabbod.githubrepolist.databinding.ItemRepoBinding
 
-class RepoListAdapter(private val items: List<Data>) :
+class RepoListAdapter(private val items: List<Data>?) :
     RecyclerView.Adapter<RepoListAdapter.RepoListViewHolder>() {
 
     inner class RepoListViewHolder(itemBinding: ItemRepoBinding) :
@@ -27,12 +27,12 @@ class RepoListAdapter(private val items: List<Data>) :
     }
 
     override fun onBindViewHolder(holder: RepoListViewHolder, position: Int) {
-        holder.tvId.text = items[position].toString()
-        holder.tvName.text = items[position].toString()
-        holder.tvDescription.text = items[position].toString()
+        holder.tvId.text = items?.get(position)?.id.toString()
+        holder.tvName.text = items?.get(position)?.name
+        holder.tvDescription.text = items?.get(position)?.description
     }
 
     override fun getItemCount(): Int {
-        return items.size
+        return items?.size ?: 0
     }
 }
